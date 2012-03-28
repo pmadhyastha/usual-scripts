42fd237169a00cd1a6bb85cf7fa1906ace4e1f7f
Pranava Swaroop <pranava.madhyastha@gmail.com>
2012-03-28 20:32:58 +0200

    imodified

--- penntowtag.pl
+++ penntowtag.pl
@@ -16,7 +16,7 @@ my @sentences = Lingua::Treebank->from_penn_file($filename);
 
 foreach (@sentences) {
   foreach ($_->get_all_terminals) {
-    print $_->word(), "/", $_->tag(), "\n";
+    print $_->word(), "/", $_->tag(), " ";
   }
 
   print "\n\n";
